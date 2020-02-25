f4e0830bb7eefb0216c570422f11b139a1b1032d
Chris Davis <chrisdavis@outlook.com>
2020-02-25 09:37:53 -0800

    Ensure control page loads and displays correct room temperature when in fahrenheit
    This change overcomes a limitation in how large a payload we can send through the http server.  This sends the header, control and footer content in chunks.  I also noticed a bug from a previous commit where we were overwriting the room temperature converted to fahrenheit with the temperature before conversion right after.

--- src/mitsubishi2mqtt/mitsubishi2mqtt.ino
+++ src/mitsubishi2mqtt/mitsubishi2mqtt.ino
@@ -552,99 +552,114 @@ void handle_control() {
 
   heatpumpSettings settings = hp.getSettings();
   settings = change_states(settings);
-  String toSend = html_common_header + html_page_control + html_common_footer;
+  String controlPage =  html_page_control;
+  String headerContent = html_common_header;
+  String footerContent = html_common_footer;
   //write_log("Enter HVAC control");
-  toSend.replace("_UNIT_NAME_", hostname);
-  toSend.replace("_VERSION_", m2mqtt_version);
-  toSend.replace("_RATE_", "60");
-  toSend.replace("_ROOMTEMP_", String(getTemperature(hp.getRoomTemperature(), useFahrenheit)));	  toSend.replace("_ROOMTEMP_", String(hp.getRoomTemperature()));
-  toSend.replace("_USE_FAHRENHEIT_", (String)useFahrenheit);
-  toSend.replace("_TEMP_SCALE_", getTemperatureScale());
+  headerContent.replace("_UNIT_NAME_", hostname);
+  footerContent.replace("_VERSION_", m2mqtt_version);
+  controlPage.replace("_UNIT_NAME_", hostname);
+  controlPage.replace("_RATE_", "60");
+  controlPage.replace("_ROOMTEMP_", String(getTemperature(hp.getRoomTemperature(), useFahrenheit)));
+  controlPage.replace("_USE_FAHRENHEIT_", (String)useFahrenheit);
+  controlPage.replace("_TEMP_SCALE_", getTemperatureScale());
 
   if (strcmp(settings.power, "ON") == 0) {
-    toSend.replace("_POWER_ON_", "selected");
+    controlPage.replace("_POWER_ON_", "selected");
   }
   else if (strcmp(settings.power, "OFF") == 0) {
-    toSend.replace("_POWER_OFF_", "selected");
+    controlPage.replace("_POWER_OFF_", "selected");
   }
 
   if (strcmp(settings.mode, "HEAT") == 0) {
-    toSend.replace("_MODE_H_", "selected");
+    controlPage.replace("_MODE_H_", "selected");
   }
   else if (strcmp(settings.mode, "DRY") == 0) {
-    toSend.replace("_MODE_D_", "selected");
+    controlPage.replace("_MODE_D_", "selected");
   }
   else if (strcmp(settings.mode, "COOL") == 0) {
-    toSend.replace("_MODE_C_", "selected");
+    controlPage.replace("_MODE_C_", "selected");
   }
   else if (strcmp(settings.mode, "FAN") == 0) {
-    toSend.replace("_MODE_F_", "selected");
+    controlPage.replace("_MODE_F_", "selected");
   }
   else if (strcmp(settings.mode, "AUTO") == 0) {
-    toSend.replace("_MODE_A_", "selected");
-  }
-  if (strcmp(settings.fan, "AUTO") == 0) {
-    toSend.replace("_FAN_A_", "selected");
-  }
-  else if (strcmp(settings.fan, "QUIET") == 0) {
-    toSend.replace("_FAN_Q_", "selected");
-  }
-  else if (strcmp(settings.fan, "1") == 0) {
-    toSend.replace("_FAN_1_", "selected");
-  }
-  else if (strcmp(settings.fan, "2") == 0) {
-    toSend.replace("_FAN_2_", "selected");
-  }
-  else if (strcmp(settings.fan, "3") == 0) {
-    toSend.replace("_FAN_3_", "selected");
-  }
-  else if (strcmp(settings.fan, "4") == 0) {
-    toSend.replace("_FAN_4_", "selected");
+    controlPage.replace("_MODE_A_", "selected");
   }
 
-  toSend.replace("_VANE_V_", settings.vane);
+  if (strcmp(settings.fan, "AUTO") == 0) {
+    controlPage.replace("_FAN_A_", "selected");
+  }
+  else if (strcmp(settings.fan, "QUIET") == 0) {
+    controlPage.replace("_FAN_Q_", "selected");
+  }
+  else if (strcmp(settings.fan, "1") == 0) {
+    controlPage.replace("_FAN_1_", "selected");
+  }
+  else if (strcmp(settings.fan, "2") == 0) {
+    controlPage.replace("_FAN_2_", "selected");
+  }
+  else if (strcmp(settings.fan, "3") == 0) {
+    controlPage.replace("_FAN_3_", "selected");
+  }
+  else if (strcmp(settings.fan, "4") == 0) {
+    controlPage.replace("_FAN_4_", "selected");
+  }
+
+  controlPage.replace("_VANE_V_", settings.vane);
   if (strcmp(settings.vane, "AUTO") == 0) {
-    toSend.replace("_VANE_A_", "selected");
+    controlPage.replace("_VANE_A_", "selected");
   }
   else if (strcmp(settings.vane, "1") == 0) {
-    toSend.replace("_VANE_1_", "selected");
+    controlPage.replace("_VANE_1_", "selected");
   }
   else if (strcmp(settings.vane, "2") == 0) {
-    toSend.replace("_VANE_2_", "selected");
+    controlPage.replace("_VANE_2_", "selected");
   }
   else if (strcmp(settings.vane, "3") == 0) {
-    toSend.replace("_VANE_3_", "selected");
+    controlPage.replace("_VANE_3_", "selected");
   }
   else if (strcmp(settings.vane, "4") == 0) {
-    toSend.replace("_VANE_4_", "selected");
+    controlPage.replace("_VANE_4_", "selected");
   }
   else if (strcmp(settings.vane, "5") == 0) {
-    toSend.replace("_VANE_5_", "selected");
+    controlPage.replace("_VANE_5_", "selected");
   }
   else if (strcmp(settings.vane, "SWING") == 0) {
-    toSend.replace("_VANE_S_", "selected");
+    controlPage.replace("_VANE_S_", "selected");
   }
-  toSend.replace("_WIDEVANE_V_", settings.wideVane);
+
+  controlPage.replace("_WIDEVANE_V_", settings.wideVane);
   if (strcmp(settings.wideVane, "<<") == 0) {
-    toSend.replace("_WVANE_1_", "selected");
+    controlPage.replace("_WVANE_1_", "selected");
   }
   else if (strcmp(settings.wideVane, "<") == 0) {
-    toSend.replace("_WVANE_2_", "selected");
+    controlPage.replace("_WVANE_2_", "selected");
   }
   else if (strcmp(settings.wideVane, "|") == 0) {
-    toSend.replace("_WVANE_3_", "selected");
+    controlPage.replace("_WVANE_3_", "selected");
   }
   else if (strcmp(settings.wideVane, ">") == 0) {
-    toSend.replace("_WVANE_4_", "selected");
+    controlPage.replace("_WVANE_4_", "selected");
   }
   else if (strcmp(settings.wideVane, ">>") == 0) {
-    toSend.replace("_WVANE_5_", "selected");
+    controlPage.replace("_WVANE_5_", "selected");
   }
   else if (strcmp(settings.wideVane, "SWING") == 0) {
-    toSend.replace("_WVANE_S_", "selected");
+    controlPage.replace("_WVANE_S_", "selected");
   }
-  toSend.replace("_TEMP_", String(getTemperature(hp.getTemperature(), useFahrenheit)));
-  server.send(200, "text/html", toSend);
+
+  controlPage.replace("_TEMP_", String(getTemperature(hp.getTemperature(), useFahrenheit)));
+
+  // We need to send the page content in chunks to overcome
+  // a limitation on the maximum size we can send at one
+  // time (approx 6k).
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", headerContent);
+  server.sendContent(controlPage);
+  server.sendContent(footerContent);
+  // Signal the end of the content
+  server.sendContent("");
   delay(100);
 }
 
