11182a91fd03096debab15b20d59657b17bd8a06
Ian Evans <ian@techne.net>
2020-04-18 08:58:51 -0700

    Changing control forms to use POST to fix #53.

--- src/mitsubishi2mqtt/config.h
+++ src/mitsubishi2mqtt/config.h
@@ -16,7 +16,7 @@
 
 //#define MY_LANGUAGE fr-FR // define your language
 
-const PROGMEM char* m2mqtt_version = "0.7.0";
+const PROGMEM char* m2mqtt_version = "0.7.1";
 
 //Define global variables for files
 #ifdef ESP32
